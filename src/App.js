1ed08f26be09020433036deeb43234c2825eee0e
Kevin Wang <kw1017@gmail.com>
2019-04-02 21:42:50 -0700

    setup api calls

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from "./components/Weather";
 
+
+const API_KEY = "4a4e671f48280eff146434b2b53bed84";
+
+
 class App extends React.Component {
+
   render() {
     return(
 
       <div>
         <Titles />
-        <Form />
+        <Form />npm
         <Weather />
       </div>
 
