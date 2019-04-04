38e5b8b594e650a827b640732e8f868f4b851b4d
Kevin Wang <kw1017@gmail.com>
2019-04-03 22:38:26 -0700

    set up state and render data to weather component

--- src/App.js
+++ src/App.js
@@ -10,15 +10,38 @@ const API_KEY = "4a4e671f48280eff146434b2b53bed84";
 
 class App extends React.Component {
 
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
+
   getWeather = async (e) => {
+    // Prevent browser refresh 
     e.preventDefault();
 
+    // Get value form the form input field
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
-
+    
+    // Weather API call
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json()
     console.log(data)
+
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: ""
+    });
+    console.log(this.state.temperature)
+
   }
 
   render() {
@@ -27,7 +50,14 @@ class App extends React.Component {
       <div>
         <Titles />
         <Form getWeather={this.getWeather} />
-        <Weather />
+        <Weather 
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}
+        />
       </div>
 
     );
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,7 +3,14 @@ import React from "react";
 class Weather extends React.Component {
     render() {
         return (
-            <div>Weather Componet</div>
+            <div>
+                {this.props.city}
+                {this.props.country}
+                {this.props.temperature}
+                {this.props.humidity}
+                {this.props.description}
+                {this.props.error}
+            </div>
         );
     }
 };
